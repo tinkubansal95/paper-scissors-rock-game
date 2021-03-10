aa0e3aec567d90b665859d340e76812047176154
Tinku Bansal <tinkubansal21@gmail.com>
2021-03-10 21:31:21 +1100

    Added code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# paper-scissors-rock-game
+## Mini-Project: Rock, Paper, Scissors
+
+
+
+## Instructions
+
+The completed application should meet the following criteria:
+
+* As a user, I want to play Rock, Paper, Scissors against an automated opponent.
+
+* As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
+
+* As a user, I expect the computer to choose R, P, or S in return.
+
+* As a user, I want the option to play again whether I win or lose.
+
+* As a user, I want to see my total wins, ties, and losses after each round.
+
---
+++ script.js
@@ -0,0 +1,42 @@
+var wins =0;
+var losses = 0;
+var ties =0;
+
+var option = ['S','P','R'];
+
+
+function game() {
+    var choice = prompt("What would you like to choose from Paper, Scissors and Rock? Please Enter P, S or R.");
+    var compChoice = option[Math.floor(Math.random() * Math.floor(option.length))];
+    choice = choice.toUpperCase();
+    if (choice === compChoice)
+    {
+        ties++;
+        window.alert("Computer choosed "+ compChoice +",It's a tie!")
+    }
+    else if (
+        choice === 'P' && compChoice === 'R' ||
+        choice === 'S' && compChoice === 'P' ||
+        choice === 'R' && compChoice === 'S'
+    )
+    {
+        wins++;
+        window.alert("Computer choosed "+ compChoice +", You Won!");
+    }
+    else{
+        losses++;
+        window.alert("Computer choosed "+ compChoice +", You Lost!");
+    }
+    window.alert("Your score:\n Total Wins: "+wins+"\nTotal Lost: "+losses+"\nTotal ties: "+ties);
+    if(window.confirm("Do you want to play again?")){
+        game();
+    }
+} 
+
+window.alert("Your final score:\n Total Wins: "+wins+"\nTotal Lost: "+losses+"\nTotal ties: "+ties);
+
+game();
+
+
+
+
